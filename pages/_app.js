5754cf2c56d44d5674571cb47a5efcf45953aeb8
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2025-03-25 18:07:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" /> */}
         <meta name="description" content="UJustBe" />
-        <title>USpacex</title>
+        <title>ORETool</title>
       </Head>
       
       {/* Render the main component */}
